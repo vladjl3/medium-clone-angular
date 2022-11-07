82bec9cc7d8277fd17964231d4b8f50c2897f598
Vladislav Gusselnikov <vladjl3@yahoo.com>
2022-11-07 19:43:10 +0600

    fix: revert feed to initial state on roter navigation, to show loading

--- src/app/shared/components/feed/store/feed.reducer.ts
+++ src/app/shared/components/feed/store/feed.reducer.ts
@@ -1,3 +1,4 @@
+import { routerNavigationAction } from '@ngrx/router-store';
 import { Action, createReducer, on } from '@ngrx/store';
 import { FeedStateInterface } from '../types/feed-state.interface';
 import {
@@ -37,7 +38,8 @@ const feedReducer = createReducer(
       ...state,
       isLoading: false,
     })
-  )
+  ),
+  on(routerNavigationAction, (): FeedStateInterface => initialState)
 );
 
 export function reducer(state: FeedStateInterface, action: Action) {
